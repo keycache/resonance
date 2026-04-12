740965840528221ce3facef2eb98b0041a2fff8c
Akash Patki <patki.aakash@gmail.com>
2026-04-12 08:54:28 -0400

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://keycache.github.io',
+  base: '/resonance', // Only if NOT using a custom domain
   vite: {
     plugins: [tailwindcss()]
   }
